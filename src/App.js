9c6193768b65cc3e12740d8e2d32e48f85743c39
Mesi <mse212002@gmail.com>
2022-04-28 08:41:07 +0300

    :sparkles: added new components, :heavy_plus_sign: added prop-types

--- src/App.js
+++ src/App.js
@@ -1,29 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import ListOfBooks from './Components/ListOfBooks';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      books: [{ id: 1, title: 'The Da Vinci Code', category: 'Fiction' },
+        { id: 2, title: 'Shogun', category: 'Historical Fiction' },
+        { id: 3, title: 'The Way of the Peaceful Warrior', category: 'Fiction, Autobiography' },
+        { id: 4, title: 'The A.B.C. Murders ', category: 'Crime Novel' }],
+    };
+  }
+
+  render() {
+    const { books } = this.state;
+    // books.map((book) => console.log(book.id))
+    // console.log(books.id)
+    return (
+      <div>
+        <ListOfBooks item={books} />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Components/ListOfBooks.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Book from './Book';
+
+const ListOfBooks = ({ item }) => {
+  <div>
+    <div>
+      <div><Book /></div>
+      <div>
+        <button type="button">Comments</button>
+        <button type="button">Remove</button>
+        <button type="button">Edit</button>
+      </div>
+    </div>
+    <div>
+      <button type="button">Completed</button>
+    </div>
+  </div>;
+  <div>
+    
+  </div>
+};
